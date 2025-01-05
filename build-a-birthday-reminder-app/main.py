728abe9b38853aedc057a9b10f85ed7999ee5e04
linda_a <larmstrongnz@gmail.com>
2025-01-06 11:14:21 +1300

    Completed build-a-birthday-reminder-app exercise

--- build-a-birthday-reminder-app/main.py
+++ build-a-birthday-reminder-app/main.py
@@ -1,1 +1,11 @@
 user_input = input("Which birthday are you looking for? ")
+
+if user_input == "Jose":
+    print("Birthday is 18/7/1981")
+elif user_input == "Nadia":
+    print("Birthday is 12/4/2011")
+elif user_input == "Martin":
+    print("Birthday is 17/11/2013")
+else: 
+    print("Can't find a birthday for this person...")
+    
