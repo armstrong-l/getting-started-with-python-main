afbce54df138c4f7db30d36b84250153d832e164
linda_a <larmstrongnz@gmail.com>
2025-01-13 18:01:36 +1300

    Completed start-your-drawing-app exercise

---
+++ start-your-drawing-app/main.py
@@ -0,0 +1,49 @@
+import turtle as turtle
+
+# turtle.pencolor("blue")
+# turtle.forward(100)
+# turtle.right(60)
+
+# turtle.pencolor("purple")
+# turtle.forward(100)
+# turtle.right(60)
+
+# turtle.pencolor("red")
+# turtle.forward(100)
+# turtle.right(60)
+
+# turtle.pencolor("orange")
+# turtle.forward(100)
+# turtle.right(60)
+
+# turtle.pencolor("yellow")
+# turtle.forward(100)
+# turtle.right(60)
+
+# turtle.pencolor("green")
+# turtle.forward(100)
+# turtle.right(60)
+
+
+# turtle.pencolor("blue")
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+
+turtle.pencolor("purple")
+
+turtle.forward(110)
+turtle.left(216)
+turtle.forward(110)
+turtle.left(216)
+turtle.forward(110)
+turtle.left(216)
+turtle.forward(110)
+turtle.left(216)
+turtle.forward(110)
+turtle.left(216)
