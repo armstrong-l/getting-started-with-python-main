8438f633aace70ac9af3f1fc2005a9ea0c030532
linda_a <larmstrongnz@gmail.com>
2025-01-14 10:21:23 +1300

    Completed design-a-guessing-game exercise

--- design-a-guessing-game/main.py
+++ design-a-guessing-game/main.py
@@ -1,11 +1,27 @@
 # import random module
+import random
+
+random_number = random.randrange(100)
 
 correct_guess = False
+
 
 while not correct_guess:
     user_input = input("Guess a number between 0 and 100: ")
 
-
+    try:
+        number = int(user_input)
+        if number == random_number:
+            correct_guess = True
+        elif number > random_number:
+            print("Your guess was too high")
+        elif number < random_number:
+            print("Your guess was too low")
+    except:
+        print("That is not a number")
 
 print("You guessed the right number!")
+
+
+
 
