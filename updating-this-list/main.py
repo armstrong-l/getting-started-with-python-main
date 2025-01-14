2f35fa539a9eeace9a4e2db5b9acf45b90f7f43d
linda_a <larmstrongnz@gmail.com>
2025-01-15 10:32:17 +1300

    Completed updating-this-list exercise

---
+++ updating-this-list/main.py
@@ -0,0 +1,16 @@
+friends = ["Nadia", "Martin", "Jose", "Steph", "Bec", "Suzy", "Ann", "Caroline", "Greg", "Peter", "Sam", "Katie", "Josh", "Eleanor", "Charlotte", "Marina", "Tim", "Nola", "Jim", "David"]
+
+dinner_friends = friends[:4]
+
+print(dinner_friends)
+
+drinks_friends = friends[16:]
+
+print(drinks_friends)
+
+hike_friends = friends[10:14]
+
+print(hike_friends)
+
+walk_friend = friends[5]
+print(walk_friend)
