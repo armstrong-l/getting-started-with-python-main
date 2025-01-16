f4dc30d777de7cc9fbe1e7f8281731eb67c7b172
linda_a <larmstrongnz@gmail.com>
2025-01-16 18:34:06 +1300

    Added to-do list and birthday functions to PersonalAssistant class

--- start-your-personal-assitant-class/main.py
+++ start-your-personal-assitant-class/main.py
@@ -5,7 +5,31 @@ class PersonalAssistant:
     self.contacts = {'Ann': 'Marketing Coordinator', 'Chelsea': 'Software Developer', 'Nichole': 'Sales Representative', 'Max': 'Technical Writer'}        
     self.todos = []
 
-  # Complete the get_contact function code
+  def add_todo(self, new_item):
+    self.todos.append(new_item)
+
+  def remove_todo(self, todo_item):
+    if todo_item in self.todos:
+    # Get the todo_item index in list
+      index = self.todos.index(todo_item)
+    # pop the index for todo_item in todos list
+      self.todos.pop(index)
+    else:
+      print("Todo is not in list!")
+
+  def get_todos(self):
+    return self.todos
+
+  def get_birthday(self, name):
+    if name == "Jose":
+      print("Birthday is 18/7/1981")
+    elif name == "Nadia":
+      print("Birthday is 12/4/2011")
+    elif user_input == "Martin":
+      print("Birthday is 17/11/2013")
+    else: 
+      print("Can't find a birthday for this person...")
+
   def get_contact(self, name):
     if name in self.contacts:
       return self.contacts[name]
@@ -15,5 +39,8 @@ class PersonalAssistant:
 
 # Code to test output of the class
 assistant = PersonalAssistant()
+assistant.add_todo("Pick up groceries")
 # Change name to one from your contacts
 print(assistant.get_contact("Ann"))
+print(assistant.get_todos())
+print(assistant.get_birthday("Jose"))
