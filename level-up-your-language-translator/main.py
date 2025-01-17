c91ee53b47474b10f0eb744111ee14b06787c34b
linda_a <larmstrongnz@gmail.com>
2025-01-17 18:15:32 +1300

    Added csv file into translator

--- level-up-your-language-translator/main.py
+++ level-up-your-language-translator/main.py
@@ -1,8 +1,16 @@
-translations = {
-  "hello":"hola",
-  "thank you":"gracias",
-  "sorry":"lo siento"
-}
+import csv
+
+translations = {}
+
+with open("translations.csv", "r") as words:
+  reader = csv.DictReader(words, delimiter=",")
+  for line in reader:
+    english = line["English"].lower()
+    spanish = line["Spanish"].lower()
+    french = line["French"].lower()
+    translations[english] = [spanish, french]
+
+
 
 done = False
 
